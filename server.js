89a7d1c2b2c411ba01a777fb03d1fe39142c6898
daniel savisky <veroullage@hotmail.fr>
2022-08-04 22:35:23 +0200

    try catch

--- server.js
+++ server.js
@@ -8,25 +8,33 @@ app.use(cors());
 const axios = require("axios");
 
 app.get("/", async (req, res) => {
-  const response = await axios.get(
-    `https://lereacteur-marvel-api.herokuapp.com/characters?apikey=${process.env.MARVEL_API_KEY}`
-  );
-  res.status(200).json(response.data);
-  console.log("ma route is good");
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apikey=${process.env.MARVEL_API_KEY}`
+    );
+    res.status(200).json(response.data);
+    console.log("ma route is good");
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 app.get("/character/:characterId", async (req, res) => {
-  const response = await axios.get(
-    `https://lereacteur-marvel-api.herokuapp.com/character/${req.params.characterId}?apiKey=${process.env.MARVERL_API_KEY}`
-  );
-  res.status(200).json(response.data);
-  console.log(response.data);
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/${req.params.characterId}?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    res.status(200).json(response.data);
+    console.log(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 app.all("*", (req, res) => {
   res.json({ message: "cette route n'existe pas" });
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(4000, () => {
   console.log("ready for use");
 });
