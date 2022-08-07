21d961b16cf3c679894c8e89f643b9b0ed09de40
daniel savisky <veroullage@hotmail.fr>
2022-08-07 16:30:39 +0200

    test heroku

--- server.js
+++ server.js
@@ -2,31 +2,69 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const mongoose = require("mongoose");
+mongoose.connect;
 
 const app = express();
 app.use(cors());
+const MARVEL_API_KEY = process.env.MARVEL_API_KEY;
 
-app.get("/characters", async (req, res) => {
-  try {
-    const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apikey=${process.env.MARVEL_API_KEY}`
-    );
-    res.status(200).json(response.data);
-    console.log("ma route is good");
-  } catch (error) {
-    console.log(error.message);
-  }
+app.get("/", (req, res) => {
+  res.status(500).json("Welcome on my Backend !");
 });
 
-app.get("/character/:characterId", async (req, res) => {
+app.get("/characters", async (req, res) => {
+  let limit = req.query.limit;
+  let skip = limit * req.query.skip;
   try {
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/character/${req.params.characterId}?apiKey=${process.env.MARVEL_API_KEY}`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=KUuKXbduDgD49DN4&limit=${limit}&skip=${skip}`
     );
     res.status(200).json(response.data);
     console.log(response.data);
   } catch (error) {
     console.log(error.message);
+  }
+});
+
+app.get("/comics/:characterId", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.characterId}?apiKey=KUuKXbduDgD49DN4`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.get("/comics", async (req, res) => {
+  let limit = req.query.limit;
+  let skip = limit * req.query.skip;
+
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=KUuKXbduDgD49DN4&limit=${limit}&skip=${skip}`
+    );
+    res.status(200).json(response.data);
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/comics/:characterId", async (req, res) => {
+  let limit = req.query.limit;
+  let skip = limit * req.query.skip;
+
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.characterId}?apiKey=KUuKXbduDgD49DN4`
+    );
+    res.status(200).json(response.data);
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
   }
 });
 
