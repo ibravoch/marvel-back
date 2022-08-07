700faed96de919412be5e6d22d12e8ffdf03082b
daniel savisky <veroullage@hotmail.fr>
2022-08-07 17:40:51 +0200

    retry

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 const MARVEL_API_KEY = process.env.MARVEL_API_KEY;
 
-app.get("/characters", async (req, res) => {
+app.get("/", async (req, res) => {
   let limit = req.query.limit;
   let skip = limit * req.query.skip;
   try {
